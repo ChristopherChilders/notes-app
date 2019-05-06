50899e4cedab5f210bed867e50c0109d2ce8eb40
Christopher Childers <christopher.childers13@yahoo.com>
2019-05-06 10:59:26 -0400

    adds some styles and some test data for us to use

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import NotesList from './NotesList'
-import NotesDetail from './NotesDetails'
+import NotesList from './NotesList';
+import NotesDetail from './NotesDetails';
+import styles from './NotesApp.module.css';
 
 export default class NotesApp extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            notes: []      // This will hold and array of objects
+            notes: [      // This will hold and array of objects
+                {
+                    id: '1001',
+                    title: 'first note',
+                    text:'Hello'
+                },
+                {
+                    id: '1002',
+                    title: 'second note',
+                    text: 'my name is jeffery'
+                },
+                {
+                    id: '1003',
+                    title: 'third note',
+                    text: 'cool, alright'
+                }
+            ]
         }
     }
     render(){
         return(
-            <div>
-                <NotesList />
+            <div className={styles.app}>
+                <div className={styles.list}>
+                <NotesList notes={this.state.notes}/>
+                </div>
                 <NotesDetail />
             </div>
         )
--- src/NotesList.js
+++ src/NotesList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import styles from './NotesList.module.css';
 
-export default function NotesList(props) {
+function NotesListItem({text}){
+    return (
+        <li>{text}</li>
+    )
+}
+
+export default function NotesList(notes) {
+    const items = notes.map(note => <NotesListItem text={note.text}/>);
     return(
-        <ul>
-            <li>This</li>
-            <li>Is</li>
-            <li>The</li>
-            <li>Notes</li>
-            <li>List</li>
+        <ul className={styles.list}>
+            {items}
         </ul>
     )
 }
